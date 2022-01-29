655562a676146aeadaeda2bee9d48aca6ef3dae9
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-01-29 15:58:31 -0300

    First commit on book's project

---
+++ lib/rumbl/accounts.ex
@@ -0,0 +1,35 @@
+defmodule Rumbl.Accounts do
+  alias Rumbl.Repo
+  alias Rumbl.Accounts.User
+  @moduledoc """
+  The Accounts context.
+  """
+
+  alias Rumbl.Accounts.User
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def list_users do
+    Repo.all(User)
+  end
+
+  def get_user(id) do
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get(User, id)
+  end
+
+  def get_user_by(params) do
+    Repo.get_by(User, params)
+  end
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+end
