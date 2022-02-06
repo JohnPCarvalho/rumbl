ed968edef92247846261d5d370f508d2faa956d2
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-02-06 18:59:15 -0300

    Add tests for user authentication

--- test/rumbl/accounts_text.exs
+++ test/rumbl/accounts_text.exs
@@ -26,11 +26,11 @@ defmodule Rumbl.AccountsTest do
 
     test "enforces unique usernames" do
       assert {:ok, %User{id: id}} = Accounts.register_user(@valid_attrs)
-      assert {:erros, changeset} = Accounts.register_user(@valid_attrs)
+      assert {:error, changeset} = Accounts.register_user(@valid_attrs)
 
       assert %{username: ["has already been taken"]} =
-        error_on(changeset)
-      assert Accounts.list_users() == []
+        errors_on(changeset)
+      assert [%User{id: ^id}] = Accounts.list_users()
     end
 
     test "requires password to be at least 6 chars long" do
@@ -40,6 +40,31 @@ defmodule Rumbl.AccountsTest do
       assert %{password: ["should be at least 6 character(s)"]} =
         errors_on(changeset)
       assert Accounts.list_users() == []
+    end
+  end
+
+  describe "authenticate_by_username_and_pass/2" do
+    @pass "123456"
+
+    setup do
+      {:ok , user: user_fixture(password: @pass)}
+    end
+
+    test "returns user with correct password", %{user: user} do
+      assert {:ok, auth_user} =
+        Accounts.authenticate_by_username_and_pass(user.username, @pass)
+
+      assert auth_user.id == user.id
+    end
+
+    test "returns unauthorized error with invalid password", %{user: user} do
+      assert {:error, :unauthorized} =
+        Accounts.authenticate_by_username_and_pass(user.username, "badpass")
+    end
+
+    test "returns not found error with no matching user for email" do
+      assert {:error, :not_found} =
+        Accounts.authenticate_by_username_and_pass("unknownuser", @pass)
     end
   end
 end
